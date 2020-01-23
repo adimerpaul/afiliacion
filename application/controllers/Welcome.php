e217ae27f15731a1b54260266df300a049cd56f7
adimer101 <adimer101@gmail.com>
2020-01-23 18:23:45 -0400

    terminado

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,44 +1,140 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 include 'tcpdf.php';
+include "qrlib.php";
 class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
 	function veri(){
-        // create new PDF document
+
+
+//        exit;
+	    $ci=$_POST['ci'];
+        $dia=$_POST['dia'];
+        $mes=$_POST['mes'];
+        $anio=$_POST['anio'];
+        $nombres= strtoupper( $_POST['nombres']);
+        $paterno=strtoupper($_POST['paterno']);
+        $materno=strtoupper($_POST['materno']);
+        $codafiliacion=0;
+        $query=$this->db->query("SELECT max(ID) as MAXIMO FROM CONSULTAAFILIACION");
+        $id=$query->row()->MAXIMO+1;
+
+	    $query=$this->db->query("SELECT *
+FROM PACIENTE P
+INNER JOIN AFILIACION A ON P.COD_PACIENTE=A.COD_PACIENTE
+WHERE P.CI='$ci' AND P.DIA='$dia' AND P.MES='$mes' AND P.GESTION='$anio'");
+
+	    if($query->num_rows()==1){
+	        $nombres=$query->row()->NOMBRE;
+            $paterno=$query->row()->A_PATERNO;
+            $materno=$query->row()->A_MATERNO;
+            $codafiliacion=$query->row()->COD_AFILIACION;
+            $text="";
+            $co="A";
+            $afiliado="SI";
+        }else{
+	        $text="NO";
+            $co="NA";
+            $afiliado="NO";
+        }
+        $codigo="OR-SSU-$co".str_pad($id, 5, "0", STR_PAD_LEFT);;
+        $mesCon=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Actubre","Noviembre","Diciembre"];
+
+        $this->db->query("INSERT INTO CONSULTAAFILIACION VALUES('$id','$nombres','$paterno','$materno','$ci','$dia','$mes','$anio','".date('m/d/Y')."','$codigo','$afiliado','$codafiliacion')");
+//        exit;
+// create new PDF document
         $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-        $pdf->SetTitle('CERTIFICADO');
-// remove default header/footer
-        $pdf->setPrintHeader(false);
-        $pdf->setPrintFooter(false);
+// set document information
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor('Adimer Paul Chambi Ajata');
+        $pdf->SetTitle('Certificado de Afiliacion');
+//        $pdf->SetSubject('TCPDF Tutorial');
+//        $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// set default header data
+        $realizado="https://ssuoruro.gob.bo/";
+        $pdf->SetHeaderData('ssu2.jpg', 20, 'CERTIFICADO DE '.$text.' AFILIACIÓN', $realizado, array(0,64,255), array(0,64,128));
+        $pdf->setFooterData(array(0,64,0), array(0,64,128));
+
+// set header and footer fonts
+        $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
 // set default monospaced font
         $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-//// set margins
-//        $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+// set margins
+        $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
-// set font
-        $pdf->SetFont('times', 'BI', 12);
-// add a page
-        $pdf->AddPage();
+// set auto page breaks
+        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
-// set some text to print
-        $txt = <<<EOD
-TCPDF Example 002
+// set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-Default page header and footer are disabled using setPrintHeader() and setPrintFooter() methods.
-EOD;
-
-// print a block of text using Write()
-        $pdf->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
+// set some language-dependent strings (optional)
+        if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+            require_once(dirname(__FILE__).'/lang/eng.php');
+            $pdf->setLanguageArray($l);
+        }
 
 // ---------------------------------------------------------
 
-//Close and output PDF document
-        $pdf->Output('example_002.pdf', 'I');
+// set default font subsetting mode
+        $pdf->setFontSubsetting(true);
+
+// Set font
+// dejavusans is a UTF-8 Unicode font, if you only need to
+// print standard ASCII chars, you can use core fonts like
+// helvetica or times to reduce file size.
+        $pdf->SetFont('dejavusans', '', 12, '', true);
+
+// Add a page
+// This method has several options, check the source code documentation for more information.
+        $pdf->AddPage();
+
+// set text shadow effect
+
+        QRcode::png("Nombre=$paterno $materno $nombres|Nacimiento=$dia de $mes Del $anio|CI=$ci|Codigo=$codigo", 'img/a.png', QR_ECLEVEL_L, 4);
+// Set some content to print
+        $html = '
+<br>
+<h2 align="center">SEGURO SOCIAL UNIVERSITARIO DE ORURO  </h2>
+<p>Oruro, '.date('d').' de '.$mesCon[(int)(date('m')-1)].' del '.date('Y').'</p>
+<h4 align="center">Formulario de '. ucfirst(strtolower($text)).' Afiliación al  <br>Seguro Social Universitario de Oruro </h4>
+<p>SEGURO SOCIAL UNIVERSITARIO DE ORURO, A SOLICITUD DEL INTERESADO: </p>
+<p align="justify">Da a conocer que: De acuerdo a la base de datos del Seguro Social Universitario de Oruro, '.$paterno.' '.$materno.' '.$nombres.', con documento de identidad '.$ci.' y fecha de nacimiento '.$dia.' de  '.strtolower($mes).' del '.$anio.', '.strtolower($text).' se encuentra afiliado (a) en nuestro seguro de salud.</p>
+<p align="center">
+<img src="img/a.png" alt="">
+</p>
+<p><b>AVISO:</b> La veracidad de la presente certificación puede ser verificada introduciendo el
+código de certificado en https://ssuoruro.gob.bo/ o a través del
+Código QR con un dispositivo móvil.</p>
+<p><b>CODIGO DE CONTROL:</b> '.$codigo.'</p>
+<p>La validez del presente documento es de 60 días a partir de la fecha de emisión.</p>
+<br>
+<br>
+<p>La reproducción total o parcial y/o el uso no autorizado de este documento, constituye un delito de ser sancionado conforme a la ley</p>
+
+';
+//        $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+// Print text using writeHTMLCell()
+        $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
+// ---------------------------------------------------------
+
+// Close and output PDF document
+// This method has several options, check the source code documentation for more information.
+        $pdf->Output('example_001.pdf', 'I');
+
+//============================================================+
+// END OF FILE
+//============================================================+
     }
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+<!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/materia/bootstrap.min.css">
     <title>Afiliacion</title>
     <style>
         label{
@@ -21,11 +21,11 @@
     <div class="row">
         <div class="col-md-10 offset-md-1">
             <div class="card mt-4">
-                <div class="card-header text-white bg-info">
-                    FORMULARIO DE AFILIACION
+                <div class="card-header text-white bg-primary">
+                    FORMULARIO DE AFILIACION SEGURO SOCIAL UNIVERSITARIO ORURO<img src="img/ssu2.png" alt="" width="50">
                 </div>
                 <div class="card-body" style="background: #EFEFEF">
-                    <form method="post" action="<?=base_url()?>Welcome/veri">
+                    <form method="post" action="<?=base_url()?>Welcome/veri" id="formulario">
                         <div class="row">
                             <div class="col-md-4">
                                 <div class="form-group">
@@ -53,7 +53,7 @@
                                             <input type="text" class="form-control" id="dia" name="dia" required placeholder="00">
                                         </div>
                                         <div class="col-sm-4">
-                                            <select name="mes" id="mes" class="form-control" required>
+                                            <select name="mes" id="mes" class="form-control"  required>
                                                 <option value="">Seleccionar..</option>
                                                 <option value="ENERO">ENERO</option>
                                                 <option value="FEBRERO">FEBRERO</option>
@@ -82,6 +82,17 @@
                                     <small class="form-text text-muted">Escribe tu Carnet de Identidad</small>
                                 </div>
                             </div>
+                            <div class="col-md-12">
+                                <div class="form-group row">
+                                    <label for="captcha" class="col-md-3 col-form-label">No soy un Robot:  </label>
+                                    <div class="col-md-2">
+                                        <div id="captcha"></div>
+                                    </div>
+                                    <div class="col-md-7">
+                                        <input type="text" class="form-control" id="textCaptcha" placeholder="Respuesta" required>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <button type="submit" class="btn btn-success btn-block">Verificar</button>
                     </form>
@@ -96,5 +107,46 @@
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script>
+    window.onload=function (e) {
+        e.preventDefault();
+        var code;
+        function createCaptcha(){
+            $('#captcha').html('');
+            var charArray="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#$%&";
+            var len=5;
+            var captcha=[];
+            for (var i=0;i<len;i++){
+                var index=Math.floor(Math.random()*charArray.length+1);
+                if (captcha.indexOf(charArray[index])==-1){
+                    captcha.push(charArray[index])
+                }else {
+                    i--;
+                }
+            }
+
+            var canv=document.createElement("canvas");
+            canv.id="captcha";
+            canv.width=100;
+            canv.height=50;
+            var ctx=canv.getContext("2d");
+            ctx.font="25px Georgia";
+            ctx.strokeText(captcha.join(""),0,30);
+            console.log(captcha.join(""));
+            code=captcha.join("");
+            $('#captcha').append(canv);
+        }
+        $('#formulario').submit(function (e) {
+            if (code!=$('#textCaptcha').val()){
+                alert('EL codigo no es el mismo');
+                createCaptcha();
+                e.preventDefault();
+                $('#textCaptcha').val('');
+                $('#textCaptcha').focus();
+            }
+        });
+        createCaptcha();
+    }
+</script>
 </body>
 </html>
